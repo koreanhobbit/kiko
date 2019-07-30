bb86d132dd37ee7b99804ca26c73399423a14a73
ferry <ferryaze1@gmail.com>
2019-07-30 14:10:18 +0900

    change footer

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -1,4 +1,5 @@
 <footer id="footer">
 	<p class="copyright">&copy;www.kiosKorea.com <br>{{ date('Y') }}</p>
+	<p class="copyright">글로벌 코리아 (GLOBAL KOREA)</p>
 	<p class="copyright">PT. Global Oasis Indonesia (GO INDONESIA)</p>
 </footer>

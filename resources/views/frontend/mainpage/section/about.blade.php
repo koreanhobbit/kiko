4e74e1c5de32ea389b1184b86449365a05e2c193
ferry <ferryaze1@gmail.com>
2019-07-30 10:55:43 +0900

    edit business reg num

--- resources/views/frontend/mainpage/section/about.blade.php
+++ resources/views/frontend/mainpage/section/about.blade.php
@@ -11,9 +11,13 @@
 				<label for="address1">
 					Korea
 				</label>
-				<p id="address1">
+				<p>
 					글로발 코리아 (Global Korea)
-					Business Registration Number: 669-20-01150 
+				</p>
+				<p>
+					Registration Number: 669-20-01150
+				</p>
+				<p id="address1">
 					Seoul Gangnam gu Yeongdong ro 513 COEX 2nd Floor Commercial Exhibition Hall A100, South Korea.
 				</p>	
 			</div>
@@ -34,9 +38,13 @@
 				<label for="address1">
 					Indonesia
 				</label>
-				<p id="address1">
+				<p>
 					PT. Global Oasis Indonesia (GO Indonesia)
+				</p>
+				<p>
 					Business Registration Number: 8120010043115
+				</p>
+				<p id="address1">
 					Ruko Madrid 2 Blok G/22 BSD Tangerang Selatan, Indonesia.
 				</p>	
 			</div>
